2b324c7f9c208e5e1d0c3f46dc0e37daabad8c7e
Ningverse <wonderning@gmail.com>
2017-03-27 09:05:30 +0800

    Create quiz34.py

---
+++ quiz34.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 题目：练习函数调用。
+
+def hello_world():
+    for i in range(3):
+        print('hello world')
+if __name__ == '__main__':
+    hello_world()
